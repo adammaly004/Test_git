82f7bef1d2794f1bcd00343d5b224d12ee75c61c
HP <adammaly004@gmail.com>
2021-07-31 10:49:59 +0200

    create activity

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from flask import flash, request
 
 
 class User(db.Model, UserMixin):
@@ -18,3 +19,51 @@ class Activity(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
+
+
+class Save:
+    def __init__(self, type):
+        self.type = type
+
+        if self.type == "tempo":
+            self.save_tempo()
+
+        elif self.type == "speed":
+            self.save_speed()
+
+        elif self.type == "long":
+            self.save_long()
+
+        elif self.type == "trot":
+            self.save_trot()
+
+        elif self.type == "intervals":
+            self.save_intervals()
+
+    def save_tempo(self):
+        flash(self.type)
+        trot_before = request.form.get("trot-before")
+        trot_after = request.form.get("trot-after")
+        main_kilometres = request.form.get("main-kilometres")
+        pace = request.form.get("pace")
+        abc = request.form.get("abc")
+        warm_up = request.form.get("warm")
+        time = request.form.get("time")
+
+        total_kilometres = int(trot_before) + \
+            int(main_kilometres) + int(trot_after)
+
+        print(
+            f"Before: {trot_before}, Type: {self.type}, After: {trot_after}, Kilometres: {main_kilometres}, Pace: {pace}, Abc: {abc}, Warm up: {warm_up}, Time: {time}, Total: {total_kilometres}")
+
+    def save_speed(self):
+        flash(self.type)
+
+    def save_long(self):
+        flash(self.type)
+
+    def save_trot(self):
+        flash(self.type)
+
+    def save_intervals(self):
+        flash(self.type)
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import User, Activity
+from .models import User, Activity, Save
 from . import db
 
 views = Blueprint("views", __name__)
@@ -22,10 +22,17 @@ def activities():
 @views.route("/create", methods=['GET', 'POST'])
 @login_required
 def create_post():
+
     if request.method == "POST":
-        types = request.form['choice']
+        # Update html file
+        choice = request.form.get('choice')
+
+        # Store informations
+        types = request.form.get('type')
+        save = Save(types)
+
     else:
-        types = ""
+        choice = ""
         flash("Choice training type", category="succes")
 
-    return render_template('create_post.html', user=current_user, type=types)
+    return render_template('create_post.html', user=current_user, type=choice)
